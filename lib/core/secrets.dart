782cfefb53b669e50f115e777c791407a3788d89
qalgile-coder <qalgile@gmail.com>
2026-09-02 19:31:38 +0200

    Update secrets and codemagic config for successful build

---
+++ lib/core/secrets.dart
@@ -0,0 +1,6 @@
+class AppSecrets {
+  static const String smtpEmail = 'qusayalgile@gmail.com';
+  static const String smtpPassword = '123456';
+  static const String cloudinaryCloudName = 'test';
+  static const String cloudinaryUploadPreset = 'test';
+}
