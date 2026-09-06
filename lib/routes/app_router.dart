8efa418f538a816f4fcecf95a3c75e5dae765caa
qalgile-coder <qalgile@gmail.com>
2026-09-06 09:17:18 +0200

    Fix localization import path for Codemagic build

--- lib/routes/app_router.dart
+++ lib/routes/app_router.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-// تم تعديل الاستيراد هنا ليكون مباشراً من مجلد l10n المحلي
-import 'l10n/app_localizations.dart';
+// تم تعديل مسار الاستيراد هنا ليكون بالصيغة الصحيحة المتوافقة مع Flutter Gen
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 import '../core/providers.dart';
 import '../models/user_model.dart';
